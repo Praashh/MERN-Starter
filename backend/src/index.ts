2a0df0869a6f07cfe63849c1ac7244d2f467cc53
Prashant Varma <prashantvarma5083@gmail.com>
2024-04-02 18:45:50 +0530

    checking

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Hello World"});
+    res.json({ message: "Hello World!"});
 });
 
 app.listen(3001, () => {
